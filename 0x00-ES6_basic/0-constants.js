468ae21a8385378ade6b399ab7996cae75bb606e
vickouma77 <Vickprestone20@gmail.com>
2023-06-26 16:12:15 +0300

    modifying taskFirst and taskLast functions

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,13 +1,15 @@
-function taskFirst() {
-    const task = 'I prefer const when I can.';
-
-    return task;
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-function taskNext() {
-    let task = 'I prefer const when I can.';
-
-    return task;
+export function getLast() {
+  return ' is okay';
 }
 
-export {taskFirst, taskNext};
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
