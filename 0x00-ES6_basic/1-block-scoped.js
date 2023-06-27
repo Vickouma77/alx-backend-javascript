aae7683ca9700dec591be4c19d5ba3d1ddd521c0
vickouma77 <Vickprestone20@gmail.com>
2023-06-27 11:58:55 +0300

    applying the eslint on 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
-  
+  const task = false;
+  const task2 = true;
+
+  //eslint-disable no-unused-vars
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const  task = true;
+    const  task2 = false;
   }
-  
+  //eslint-enable no-unused-vars
+
   return [task, task2];
 }
-  
