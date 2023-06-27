6c19b489580a4fb1487bca4c9c8590d75422a6d0
vickouma77 <Vickprestone20@gmail.com>
2023-06-27 12:18:26 +0300

    applying the eslint on 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
-
   if (trueOrFalse) {
     const taskInner = true;
     const task2Inner = false;
     return [taskInner, task2Inner];
   }
-
   return [task, task2];
 }
