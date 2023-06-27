4231583c892a04da43b50d777978316505b0083d
vickouma77 <Vickprestone20@gmail.com>
2023-06-27 16:27:53 +0300

    condensing the function to 1 line

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,2 +1,3 @@
-export default getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => {return initialNumber + expansion1989 + expansion2019;};
-
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
