8eaa3121a77a62a109e8c90d1e90203ef08d92d5
vickouma77 <Vickprestone20@gmail.com>
2023-06-27 22:21:48 +0300

    computed property names on the budget object

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,12 +1,14 @@
 function getCurrentYear() {
-  const date = new Date();
-  return date.getFullYear();
-}
+    const date = new Date();
+    return date.getFullYear();
+  }
+  
+  export default function getBudgetForCurrentYear(income, gdp, capita) {
+    return {
+      [`income-${getCurrentYear()}`]: income,
+      [`gdp-${getCurrentYear()}`]: gdp,
+      [`capita-${getCurrentYear()}`]: capita,
+    };
+  }
 
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-  return {
-    [`income-${getCurrentYear()}`]: income,
-    [`gdp-${getCurrentYear()}`] : gdp,
-    [`capita-${getCurrentYear()}`] : capita,
-  };
-}
+  console.log(getBudgetForCurrentYear(2100, 5200, 1090));
