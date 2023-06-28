f6e6976eb29b3b2886eb9e1835b5060970125d55
vickouma77 <Vickprestone20@gmail.com>
2023-06-28 17:18:03 +0300

    Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,11 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
+    /* eslint-disable no-constant-condition */
     if (true) {
       resolve();
-    } else {
+    }
+    /* eslint-enable no-constant-condition */
+    else {
       reject();
     }
   });
