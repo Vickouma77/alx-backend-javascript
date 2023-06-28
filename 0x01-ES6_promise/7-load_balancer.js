366281215b18d9d760c71534fc010437a6407e44
vickouma77 <Vickprestone20@gmail.com>
2023-06-28 23:01:43 +0300

    value returned by the promise that resolved the first.

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return promise.race([chinaDownload, USDownload])
+  return Promise.race([chinaDownload, USDownload])
     .then((value) => value);
 }
