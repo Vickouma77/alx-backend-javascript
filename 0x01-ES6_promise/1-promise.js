1b346af576204de9d5576a3ffd0358a75f8b68c7
vickouma77 <Vickprestone20@gmail.com>
2023-06-28 20:24:00 +0300

    handling promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,10 @@
 export default function getFullResponseFromAPI(success) {
   const myPromise = new Promise((resolve, reject) => {
     if (success) {
-      resolve({status: 200, body: 'Success'});
+      resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
   return myPromise;
-
 }
