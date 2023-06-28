954cea93ff1cffd0c8be564791f5df1a7a1510a1
vickouma77 <Vickprestone20@gmail.com>
2023-06-28 18:17:25 +0300

    returning a promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({status: 200, body: 'Success'});
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
