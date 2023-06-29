22e25f1f2ec059a6e68fad53652001fa6e8c1f29
vickouma77 <Vickprestone20@gmail.com>
2023-06-29 18:17:19 +0300

    Implementing a class named HolbertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -30,14 +30,23 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be set to a string');
+    }
     this._name = newName;
   }
 
   set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be set to a number');
+    }
     this._length = newLength;
   }
 
   set students(newStudents) {
+    if (typeof newStudents !== 'object') {
+      throw new TypeError('Students must be set to an array of strings');
+    }
     this._students = newStudents;
   }
 }
