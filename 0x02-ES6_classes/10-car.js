36ae821831ff05896be81d03f4b538494ce579db
vickouma77 <Vickprestone20@gmail.com>
2023-06-29 23:26:39 +0300

    Implementing a class named Car

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -6,6 +6,6 @@ export default class Car {
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
