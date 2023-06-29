bc46672aa8d14e93f31cbb550be7879d99a0d433
vickouma77 <Vickprestone20@gmail.com>
2023-06-29 17:57:35 +0300

    Implementing a class named HolbertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,7 +1,31 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = name;
-        this._length = length;
-        this._students = students
-    }
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    this._length = newLength;
+  }
+
+  set students(newStudents) {
+    this._students = newStudents;
+  }
 }
