07199974ba0dec91e46b31f204baaab88d8266a9
vickouma77 <Vickprestone20@gmail.com>
2023-06-29 19:01:13 +0300

    Implementing a method named displayFullCurrency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -7,10 +7,28 @@ export default class Currency { /* eslint-disable */
         }
         if (typeof name !== 'string') {
             throw new TypeError('Name must be a string')
-        }
+        }else {
+            this._name = name
+        }   
+    }
 
-        function displayFullCurrency() {
-            return `${this._name} (${this._code})`
+    get code () {
+        return this._code
+    }
+
+    set code (newCode) {
+        if (typeof code !== 'string') {
+            throw new TypeError('Code must be a string')
+        }
+    }
+
+    get name () {
+        return this._name
+    }
+
+    set name (newName) {
+        if (typeof newName !== 'string') {
+            throw new TypeError('Name must be a string')
         }
     }
 }
