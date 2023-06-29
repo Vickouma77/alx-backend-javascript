1a430f9d6b7d99efc7bd814058de820076b4a5d3
vickouma77 <Vickprestone20@gmail.com>
2023-06-29 22:48:02 +0300

    Implementin a class named Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,10 +1,10 @@
 export default class Airport {
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    toString() {
-        return `[object ${this._code}]`;
-    }
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
