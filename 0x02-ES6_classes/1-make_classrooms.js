ae10107e1c0d5620e591523f308e1e988c280031
vickouma77 <Vickprestone20@gmail.com>
2023-06-29 17:09:51 +0300

    Implementing a function named initializeRooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,11 @@
 import ClassRoom from './0-classroom.js';
 
-export default function initializeRooms(){
+export default function initializeRooms() {
   const rooms = [
     new ClassRoom(19),
     new ClassRoom(20),
     new ClassRoom(34)
-
   ];
-
   return (rooms);
 }
 
