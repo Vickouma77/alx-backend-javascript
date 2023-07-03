7291125ea946dad94542992eaa8e23c806ab4593
vickouma77 <Vickprestone20@gmail.com>
2023-07-03 18:29:48 +0300

    .

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,8 @@
-export default function getListStudents() {
+function getListStudents() {
   return [
     {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
     {id: 2, firstname: 'James', location: 'Colombia'},
     {id: 5, firstName: 'Serena', location: 'San Francisco'}];
 }
+
+export default getListStudents;
