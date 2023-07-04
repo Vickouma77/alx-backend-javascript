8ec401db3d5ebb8a4cf03b32256f77b808b01b54
vickouma77 <Vickprestone20@gmail.com>
2023-07-04 16:20:19 +0300

    removing && condition in the if statement

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 const cleanSet = (set, start) => {
   const newSet = new Set();
   set.forEach((value) => {
-    if (value.startsWith(start)) {
+    if (value.startsWith(start) && start !== '') {
       newSet.add(value.slice(start.length));
     }
   });
