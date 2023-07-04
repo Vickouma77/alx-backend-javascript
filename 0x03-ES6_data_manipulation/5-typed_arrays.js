2e017fb7218eb4efe5ebab313d1e3ec848df4a1e
vickouma77 <Vickprestone20@gmail.com>
2023-07-04 23:57:36 +0300

    returns a new ArrayBuffer with an Int8

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
-function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position > length - 1) {
-        throw Error ('Position outside range');
-    }
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+  const newBuffer = new ArrayBuffer(length);
+  const int8 = new Int8Array(newBuffer, 0, length);
+  int8.set([value], position);
+  return new DataView(newBuffer);
 }
