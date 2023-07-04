b696efd515e62c310e643460aa4057fc1205b3f5
vickouma77 <Vickprestone20@gmail.com>
2023-07-04 18:54:25 +0300

    function that returns a map of groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+const groceriesList = () => {
+  const gro = new Map();
+  gro.set('Apples', 10);
+  gro.set('Tomatoes', 10);
+  gro.set('Pasta', 1);
+  gro.set('Rice', 1);
+  gro.set('Banana', 5);
+  return gro;
+};
+
+export default groceriesList;
