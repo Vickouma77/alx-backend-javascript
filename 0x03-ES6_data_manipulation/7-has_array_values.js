24642ecc3eea0bf622736f7f5a5cdebfb9626dd5
vickouma77 <Vickprestone20@gmail.com>
2023-07-04 15:05:21 +0300

    function that returns a boolean if all the elements in the array exist within the set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,6 @@
 const hasValuesFromArray = (arr, values) => {
-  if (!Array.isArray(arr) || !Array.isArray(values)) {
-    return false;
-  }
-  return values.every((item) => arr.includes(item));
+  const set = new Set(arr);
+  return values.every((value) => set.has(value));
 };
 
 export default hasValuesFromArray;
