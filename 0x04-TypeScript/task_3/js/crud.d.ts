8fba61dd3b0e5bc21166eb61496a92c7eb293968
vickouma77 <Vickprestone20@gmail.com>
2023-07-05 22:59:24 +0300

    update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -3,3 +3,11 @@ import { RowID, RowElement} from './interface';
 declare function insertRow(row: RowElement) : number;
 declare function deleteRow(rowId: RowID): undefined;
 declare function updateRow(rowId: RowID, row: RowElement): number;
+
+declare const CRUD: {
+    insertRow: typeof insertRow;
+    deleteRow: typeof deleteRow;
+    updateRow: typeof updateRow;
+}
+
+export default CRUD
