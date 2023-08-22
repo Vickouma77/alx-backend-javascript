8a94fc759e4d1bececcd816bd5eb04dddee120db
vickouma77 <Vickprestone20@gmail.com>
2023-08-22 18:17:41 +0300

    more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,34 +2,71 @@
 
 const http = require('http');
 const { readFile } = require('fs');
-const countStudents = require('./3-read_file_async');
 
-const port = 1245;
 const hostname = '127.0.0.1';
+const port = 1245;
 
-const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    readFile(process.argv[2], 'utf-8', (err, data) => {
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
       if (err) {
-        res.end('This is the list of our students\n');
+        reject(err);
       } else {
-        countStudents(process.argv[2])
-          .then((value) => {
-            res.end(`Number of students: ${value.length}\n${data}`);
-          })
-          .catch((error) => {
-            res.end(error.message);
-          });
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
       }
     });
+  });
+}
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  if (request.url === '/') {
+    response.write('Hello Holberton School!');
+    response.end();
   }
-}).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  if (request.url === '/students') {
+    response.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      response.end(outString);
+    }).catch(() => {
+      response.statusCode = 404;
+      response.end('Cannot load the database');
+    });
+  }
+});
+
+app.listen(port, hostname, () => {
 });
 
 module.exports = app;
