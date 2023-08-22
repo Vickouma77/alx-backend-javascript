7a21e308028f604e2f2c273dad4995a2368b0093
vickouma77 <Vickprestone20@gmail.com>
2023-08-22 17:26:01 +0300

    a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+}).listen(1245);
+
+module.exports = app;
