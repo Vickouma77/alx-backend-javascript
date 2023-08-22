c35d96dce45c19fe4dfdc998b53e29d0fe30655e
vickouma77 <Vickprestone20@gmail.com>
2023-08-22 18:12:30 +0300

    more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,20 +2,34 @@
 
 const http = require('http');
 const { readFile } = require('fs');
+const countStudents = require('./3-read_file_async');
 
 const port = 1245;
 const hostname = '127.0.0.1';
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    readFile(process.argv[2], 'utf-8', (err, data) => {
-        if (err) {
-        res.end(err.message);
-        } else {
-        res.end(data);
-        }
-    });
-    }).listen(port, hostname, () => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello Holberton School!');
+    }
+    if (req.url === '/students') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        readFile(process.argv[2], 'utf-8', (err, data) => {
+            if (err) {
+                res.end('This is the list of our students\n');
+            } else {
+                countStudents(process.argv[2])
+                    .then((value) => {
+                        res.end(`Number of students: ${value.length}\n${data}`);
+                    })
+                    .catch((error) => {
+                        res.end(error.message);
+                    });
+            }
+        });
+    }
+}).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+module.exports = app;
