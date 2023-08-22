8c3ada956a6cf439476fd670c0f1c24dfbd6dad5
vickouma77 <Vickprestone20@gmail.com>
2023-08-22 18:13:45 +0300

    more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,28 +8,28 @@ const port = 1245;
 const hostname = '127.0.0.1';
 
 const app = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello Holberton School!');
-    }
-    if (req.url === '/students') {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        readFile(process.argv[2], 'utf-8', (err, data) => {
-            if (err) {
-                res.end('This is the list of our students\n');
-            } else {
-                countStudents(process.argv[2])
-                    .then((value) => {
-                        res.end(`Number of students: ${value.length}\n${data}`);
-                    })
-                    .catch((error) => {
-                        res.end(error.message);
-                    });
-            }
-        });
-    }
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  }
+  if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    readFile(process.argv[2], 'utf-8', (err, data) => {
+      if (err) {
+        res.end('This is the list of our students\n');
+      } else {
+        countStudents(process.argv[2])
+          .then((value) => {
+            res.end(`Number of students: ${value.length}\n${data}`);
+          })
+          .catch((error) => {
+            res.end(error.message);
+          });
+      }
+    });
+  }
 }).listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
