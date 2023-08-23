ea1da8ddd7156b591abc6e3b318d8e3684b069e6
vickouma77 <Vickprestone20@gmail.com>
2023-08-23 23:02:10 +0300

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const calculateNumber = (type, a, b) => {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    } if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    } if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+    return 'Error';
+  };
+  
+  module.exports = calculateNumber;
+  
