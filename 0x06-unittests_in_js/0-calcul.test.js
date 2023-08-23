00e91e3a4d17fd8aaaf86180a9aaefc3e2556208
vickouma77 <Vickprestone20@gmail.com>
2023-08-23 18:43:52 +0300

    test for calculateNumber function

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+    it('should return 4 when adding 1 and 3', () => {
+        assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should return 5 when adding 1 and 3.7', () => {
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('should return 5 when adding 1.2 and 3.7', () => {
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should return 6 when adding 1.5 and 3.7', () => {
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+    });
+});
