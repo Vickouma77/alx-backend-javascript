526c1cb65eaad621835620a2b4a18d5134c6df11
vickouma77 <Vickprestone20@gmail.com>
2023-08-23 18:58:10 +0300

    package.json

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -18,4 +18,12 @@ describe('calculateNumber', () => {
   it('should return 6 when adding 1.5 and 3.7', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+
+  it('should return 0 when adding 0.1 and 0.3', () => {
+    assert.strictEqual(calculateNumber(0.1, 0.3), 0);
+  });
+
+  it('should return -1 when adding -0.1 and -0.3', () => {
+    assert.strictEqual(calculateNumber(-0.1, -0.3), -1);
+  });
 });
