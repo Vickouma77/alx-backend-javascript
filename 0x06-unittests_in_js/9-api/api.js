2b2fa8178ec19522764c1a63b6644d7f896e249d
vickouma77 <Vickprestone20@gmail.com>
2023-08-24 23:19:06 +0300

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
 });
