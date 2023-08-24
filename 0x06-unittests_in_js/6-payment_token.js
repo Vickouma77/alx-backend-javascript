7453cc5de1f55c099524ea19faf92fe45826fe45
vickouma77 <Vickprestone20@gmail.com>
2023-08-24 18:32:26 +0300

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const getPaymentTokenFromAPI = (success) => {
+  if (success === true) {
+    return Promise.resolve({ data: 'Successful response from the API' });
+  }
+
+  return null;
+};
+
+module.exports = getPaymentTokenFromAPI;
