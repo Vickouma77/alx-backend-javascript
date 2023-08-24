fd2f6abfcba899f9a1206268220b15da970b0a8d
vickouma77 <Vickprestone20@gmail.com>
2023-08-24 15:33:39 +0300

    Spies

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+};
+
+module.exports = sendPaymentRequestToApi;
