2a4dc7455eeb7b329bf537f20b16beb18972e53d
vickouma77 <Vickprestone20@gmail.com>
2023-08-24 23:11:49 +0300

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,9 +1,28 @@
+const request = require('request');
 const { describe, it } = require('mocha');
 const { expect } = require('chai');
-const app = require('./api');
 
-describe('Testing the API endpoint', () => {
-  it('GET /', () => {
-    expect(app).to.be.a('function');
+describe('Index page', () => {
+  const options = {
+    url: 'http://localhost:7865/',
+    method: 'GET',
+  };
+  it('check correct status code', (done) => {
+    request(options, (err, res, body) => {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+    });
+  });
+  it('check correct content', (done) => {
+    request(options, (err, res, body) => {
+	    expect(body).to.contain('Welcome to the payment system');
+	    done();
+    });
+  });
+  it('check correct content length', (done) => {
+    request(options, (err, res, body) => {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+    });
   });
 });
