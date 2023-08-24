adee9c3a97f364244e3f0f34ad32af876e613372
vickouma77 <Vickprestone20@gmail.com>
2023-08-24 23:16:40 +0300

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const express = require('express');
+
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
